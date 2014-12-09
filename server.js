44ce7fd7abc05a8ed275c8b29a0aaca03871eef5
Alexander Deschamps <alexadeschamps@gmail.com>
2014-12-09 15:39:51 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var WebSocketServer = require("ws").Server;
+var server = new WebSocketServer({port: 3000});
+
+var userDb = [];
+
+server.on("connection", function(wsc){
+	wsc.send("Connected");
+	var user = {
+		name: "",
+		client: wsc
+	}
+	client.on("message", function(message){
+		if(this.name === ""){
+			this.name = message
+		}else{
+			userDb.forEach(function(user){
+				if(user.client != this){
+					user.client.send(message);
+				}
+			}
+		}
+	})
+	userDb.push(user);
+})
